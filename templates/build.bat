cd5be5c88f61065c6367cbbb4b2ce17fac03eb8d
李文龙 <285753421@qq.com>
2020-02-11 23:35:47 +0800

    build

--- templates/build.bat
+++ templates/build.bat
@@ -5,12 +5,6 @@ set GOPROXY=https://goproxy.cn,direct
 go env -w GO111MODULE=auto
 go env -w GOMOD=%nowDir%"/go.mod"
 go env -w GOPROXY=https://goproxy.cn,direct
-if exist go.sum (
-    del go.sum
-)
-
-:: go get github.com/swaggo/swag/cmd/swag
-:: %GOPATH%/bin/swag init
 
 go mod init {{package}}
 go mod vendor
--- templates/build.sh
+++ templates/build.sh
@@ -4,10 +4,6 @@ current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 export GO111MODULE="auto"
 export GOMOD=${current_path}"/go.mod"
 
-rm go.sum
-
-go get github.com/swaggo/swag/cmd/swag
-# ${GOPATH}/bin/swag init
 go mod init {{package}}
 go mod vendor
 go run main.go
