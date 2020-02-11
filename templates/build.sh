90c73672ba88979f35efcf2d784d2d47c7d1c5d9
李文龙 <285753421@qq.com>
2020-02-11 23:54:22 +0800

    templatws

--- templates/build.sh
+++ templates/build.sh
@@ -4,6 +4,18 @@ current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 export GO111MODULE="auto"
 export GOMOD=${current_path}"/go.mod"
 
+if [ -f "go.mod" ]; then
+  rm -f go.mod
+fi
+
+if [ -f "go.sum" ]; then
+  rm -f go.sum
+fi
+
+if [ ! -d "vendor" ];then
+  rm -rf vendor
+fi
+
 go mod init {{package}}
 go mod vendor
 go run main.go
