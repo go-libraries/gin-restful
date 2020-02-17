250755161df4597c53092783d72af9cf751ffef4
李文龙 <285753421@qq.com>
2020-02-17 09:10:56 +0800

    build

--- templates/build.sh
+++ templates/build.sh
@@ -1,21 +1,15 @@
 #/bin/bash
 current_path=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-
+cd $current_path;
 export GO111MODULE="auto"
 export GOMOD=${current_path}"/go.mod"
 
-if [ -f "go.mod" ]; then
-  rm -f go.mod
-fi
-
-if [ -f "go.sum" ]; then
-  rm -f go.sum
+if [ ! -f "go.mod" ]; then
+  go mod init {{package}}
 fi
 
 if [ ! -d "vendor" ];then
-  rm -rf vendor
+  go mod vendor
 fi
 
-go mod init {{package}}
-go mod vendor
 go run main.go
